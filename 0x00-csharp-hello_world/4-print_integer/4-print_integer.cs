87b091f9833ca349145538592f8b8b7f9e6ff465
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-13 18:12:45 -0500

    fix task 4

--- 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
+++ 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 
-namespace _4_print_integer
+class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int number = 972;
-            Console.WriteLine("{0} Mission Street", number);
-        }
-    }
+	static void Main(string[] args)
+	{
+		int number = 972;
+		Console.WriteLine("{0} Mission Street", number);
+	}
 }
