cc827de1b53a05bc029721e7a59e7ffdbd11869e
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-13 18:05:53 -0500

    feature task 4

---
+++ 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4_print_integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 972;
+            Console.WriteLine("{0} Mission Street", number);
+        }
+    }
+}
