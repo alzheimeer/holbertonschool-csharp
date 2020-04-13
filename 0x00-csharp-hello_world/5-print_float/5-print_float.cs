294f2a124430a417b7df785c62a7b7d309f55d18
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-13 18:49:00 -0500

    fix task 5

--- 0x00-csharp-hello_world/5-print_float/5-print_float.cs
+++ 0x00-csharp-hello_world/5-print_float/5-print_float.cs
@@ -3,8 +3,8 @@
 class Program
 {
 	static void Main(string[] args)
-        {
-		    float number = 3.14159f;
-            Console.WriteLine("Float: {0:2N}", number);
-        }
+    {
+        float number = 3.14159f;
+        Console.WriteLine("Float: {0:N2}", number);
+    }
 }
