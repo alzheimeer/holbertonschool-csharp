a1f33e80cf98361a10711ad8e7fe5210f33c96ff
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-14 07:39:48 -0500

    feature task 2

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 97;
+            for(; a<=122; a++)
+                Console.Write((char)a);
+        }
+    }
+}
