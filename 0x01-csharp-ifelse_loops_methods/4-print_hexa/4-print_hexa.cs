6fb78851f12d443e97ed8c74a7883496d58638e1
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-14 08:44:27 -0500

    featrure task 4

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i<=98; i++)
+                Console.Write("{0} = 0x{0:x}\n",i);
+        }
+    }
+}
