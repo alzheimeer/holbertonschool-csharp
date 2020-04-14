c63cd77468f6d04b5d37b760f0a5e135337e7298
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-14 10:07:24 -0500

    feature task 8 and 9

---
+++ 0x01-csharp-ifelse_loops_methods/9-add/9-main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("{0}", Number.Add(1, 2));
+        Console.WriteLine("{0}", Number.Add(98, 0));
+        Console.WriteLine("{0}", Number.Add(100, -2));
+    }
+}
