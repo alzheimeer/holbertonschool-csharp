17c09a922fe21fc875471d4d2660d6996d9f73cd
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-13 21:52:20 -0500

    feature task 9

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -7,6 +7,6 @@ class Program
         double percent = .7553;
 	    double currency = 98765.4321;
         Console.WriteLine("Percent: {0:P2}", percent);
-	    Console.WriteLine("Currency: {0:C}", currency);
+	    Console.WriteLine("Currency: {0:C2}", currency);
     }
 }
