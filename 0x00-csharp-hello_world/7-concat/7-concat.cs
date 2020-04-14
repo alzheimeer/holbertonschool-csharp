1ab1019ed5ecd60750fa9062297f5dc9cb7fa62a
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-13 21:05:57 -0500

    feature task 7

---
+++ 0x00-csharp-hello_world/7-concat/7-concat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str1 = "Holberton";
+		string str2 = "School";
+		str1 = str1 + str2;
+		Console.WriteLine("Welcome to {0}!", str1);
+	}
+}
