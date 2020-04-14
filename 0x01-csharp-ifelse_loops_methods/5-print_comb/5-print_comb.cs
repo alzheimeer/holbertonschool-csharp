85c66a072cc7d93493285acd055e820c60045ebb
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-14 08:53:20 -0500

    feature task 5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i<=99; i++)
+                {
+                    if(i!=99)
+                        Console.Write("{0:D2}, ", i);
+                    else
+                        Console.Write("{0:D2}\n", i);
+                }
+        }
+    }
+}
