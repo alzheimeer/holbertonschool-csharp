e46638c77bad9b18dc71c7791d37b6d3c4b77152
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-14 01:03:26 -0500

    feature task 0

---
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        if (number > 0)
+            Console.WriteLine("{0} is positive", number);
+        else if (number < 0)
+            Console.WriteLine("{0} is negative", number);
+        else
+            Console.WriteLine("{0} is zero", number);
+    }
+}
