2b522b06c97a6cc1cb5ace0d63cac811ef0d73f5
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-14 10:38:47 -0500

    feature task 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class ExecutePrintDiagonal
+{
+    static void Main(string[] args)
+    {
+        Line.PrintDiagonal(3);
+        Line.PrintDiagonal(0);
+        Line.PrintDiagonal(12);
+        Line.PrintDiagonal(-98);
+    }
+}
---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if(length > 0)
+        {
+            int i, j;
+            for(i=0; i<length;i++)
+            {
+                for(j = 0; j<i;j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("\\");
+            }
+        }
+        Console.WriteLine(""); 
+    }
+}
