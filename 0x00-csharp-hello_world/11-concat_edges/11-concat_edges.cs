238c428ec3ad1d771a1c80f1c0d14c34aa885824
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-13 23:02:43 -0500

    feature task eleven

---
+++ 0x00-csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
+		str = str.Substring(49, 15) + ' ' + str.Substring(80, 11) + ' ' + str.Substring(88, 2) + ' ' +str.Substring(0, 2);
+		Console.WriteLine(str);
+        }
+}
