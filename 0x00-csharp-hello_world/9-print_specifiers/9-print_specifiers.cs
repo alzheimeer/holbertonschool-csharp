021001d0b6942326f687a67a48f586a2b237525e
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-13 21:50:30 -0500

    feature tash 9

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -2,11 +2,11 @@
 
 class Program
 {
-	static void Main(string[] args)
-	{
-		double percent = .7553;
-		double currency = 98765.4321;
-		Console.WriteLine("Percent: {0:P}", percent);
-		Console.WriteLine("Currency: {0:C}", currency);
-	}
+    static void Main(string[] args)
+    {
+        double percent = .7553;
+	    double currency = 98765.4321;
+        Console.WriteLine("Percent: {0:P2}", percent);
+	    Console.WriteLine("Currency: {0:C}", currency);
+    }
 }
