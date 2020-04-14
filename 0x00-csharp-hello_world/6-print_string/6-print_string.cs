951b44b824d146fa8663e0cd74685ff73982f51d
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-13 20:59:56 -0500

    feature task 6

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+        Console.WriteLine(str+str+str+"\n"+str.Substring(0,9));
+    }
+}
