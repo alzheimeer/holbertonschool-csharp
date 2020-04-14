26fae702ffa92ce390d7130bc48a5841964f1e11
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-14 09:28:50 -0500

    feature task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i<=99; i++)
+                {
+                    int d1 = i % 10;
+                    int d2 = i / 10;
+                    if(d1 != d2 && d1 > d2)
+                    {
+                        if(i!=99)
+                            Console.Write("{0:D2}, ", i);
+                        else
+                            Console.Write("{0:D2}\n", i);
+                    }
+                }
+        }
+    }
+}
