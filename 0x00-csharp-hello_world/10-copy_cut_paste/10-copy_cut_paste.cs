55b0abd068d3419be445ab02feca0912d25accbc
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-13 22:40:45 -0500

    feature task ten

---
+++ 0x00-csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+        static void Main(string[] args)
+        {
+		string word = "Holberton";
+		string word_first_3 = word.Substring(0, 3);
+        string word_last_2 = word.Substring(7);
+        string middle_word = word.Substring(1, 7);
+        Console.WriteLine("First 3 letters: {0}", word_first_3);
+		Console.WriteLine("Last 2 letters: {0}", word_last_2);
+		Console.WriteLine("Middle word: {0}", middle_word);
+        }
+}
