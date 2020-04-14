45f5902207c1e5bc4b28c28ed27fc670d092941c
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-14 08:30:12 -0500

    feature task 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(char a= 'a'; a<='z'; a++)
+            {
+               if(a!='q' && a!='e')
+                    Console.Write(a);
+            }
+        }
+    }
+}
