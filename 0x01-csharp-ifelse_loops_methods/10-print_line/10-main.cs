951659ed5e7f062f1dc775e2e12fb945552091c8
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-14 10:18:31 -0500

    feature task 10

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Line.PrintLine(3);
+        Line.PrintLine(0);
+        Line.PrintLine(12);
+        Line.PrintLine(-98);
+    }
+}
---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if(length > 0)
+        {
+            for(int i=0; i<length;i++)
+                Console.Write("_");
+        }
+        Console.WriteLine("");
+    }
+}
