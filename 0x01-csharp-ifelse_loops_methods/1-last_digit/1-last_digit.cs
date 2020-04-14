a0d49c2c75f272011dbc248ae7a08b528062ebcf
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-14 12:52:23 -0500

    fix task 1

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -9,9 +9,9 @@ class Program
         int ud = number % 10; 
         if (ud > 5)
             Console.WriteLine("The last digit of {0} is {1} and is greater than 5", number, ud);
-        else if (ud < 6)
-            Console.WriteLine("The last digit of {0} is {1} and is less than 6 and not 0", number, ud);
         else if (ud == 0)
             Console.WriteLine("The last digit of {0} is {1} and is 0", number, ud);
-    }
+        else
+            Console.WriteLine("The last digit of {0} is {1} and is less than 6 and not 0", number, ud);
+        }
 }
