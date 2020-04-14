6d19057a513d07e8e080ce447e49218080cbc382
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-13 21:18:30 -0500

    feature task 8

---
+++ 0x00-csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "Street";
+        string str2 = "Mission";
+        int int1 = 972;
+        Console.WriteLine("{2} {1} {0}", str1,str2,int1);
+    }
+}
