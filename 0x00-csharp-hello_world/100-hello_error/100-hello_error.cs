5826ce6a9ce22799acbddf969601c88a60749b5b
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-13 23:20:37 -0500

    feature task adv100

---
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _100_hello_error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Error.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+            Environment.Exit(1);
+        }
+    }
+}
