6699fa34e1f09ab3648961086fcf39d357458a42
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-13 22:43:58 -0500

    feature task ten

--- 0x00-csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
+++ 0x00-csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -6,8 +6,8 @@ class Program
         {
 		string word = "Holberton";
 		string word_first_3 = word.Substring(0, 3);
-        string word_last_2 = word.Substring(7);
-        string middle_word = word.Substring(1, 7);
+        string word_last_2 = word.Substring(word.Length - 2, 2);
+        string middle_word = word.Substring(1, word.Length - 2);
         Console.WriteLine("First 3 letters: {0}", word_first_3);
 		Console.WriteLine("Last 2 letters: {0}", word_last_2);
 		Console.WriteLine("Middle word: {0}", middle_word);
