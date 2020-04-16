c92a6b25058ee130d2dd91d031741e9dff674641
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-16 13:57:35 -0500

    fix task 1

--- 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -4,7 +4,7 @@ class Array
 {
     public static int elementAt(int[] array, int index)
     {
-        if(index < 0 || index > array.Length)
+        if(index < 0 || index > array.Length - 1)
             { 
             Console.WriteLine("Index out of range");
             return -1;
