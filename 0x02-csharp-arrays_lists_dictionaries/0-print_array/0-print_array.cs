2d4b00eac7849bad77eedf18757946492adccdbd
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-16 13:20:30 -0500

    Fix task 0

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -9,6 +9,12 @@ class Array
             Console.WriteLine("Size cannot be negative");
             return null;
         }
+        if (size == 0)
+	    {
+	        Console.WriteLine();
+	        return index;
+        }
+
         var array = new int[size];
         for(int i=0; i < size;i++) 
             array[i] =  i; 
