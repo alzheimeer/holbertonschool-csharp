fd149d7a1661778e0337511d08dfd130ba2d7b98
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-16 15:33:26 -0500

    Feature task 5

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using System.Linq;
+using System;
 using System.Collections.Generic;
 
 class List
 {
     public static int MaxInteger(List<int> myList)
     {
+        if(myList.Count == 0)
+        {
+        Console.WriteLine("List is empty");
+        return -1;
+        }
+
         int eMax = 0;
         foreach (int i in myList)
         {
