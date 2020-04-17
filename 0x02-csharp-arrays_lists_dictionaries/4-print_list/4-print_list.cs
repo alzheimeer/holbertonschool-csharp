e607971228091af02ff3824d0290a5797be845b4
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-16 21:32:46 -0500

    fix 4 ternario

--- 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -10,27 +10,18 @@ class List
             Console.WriteLine("Size cannot be negative");
             return null;
         }
-        
         List<int> listA = new List<int>();
-
         if (size == 0)
 	    {
 	        Console.WriteLine();
 	        return listA;
         }
-
-        
         for(int i=0; i < size;i++) 
             {
-                if(i != size-1)
-                    Console.Write("{0} ",i);
-                else
-                    Console.Write(i);
+                Console.Write("{0}{1}",i , i != size-1 ? " ":"");
                 listA.Add(i); 
-                
             }
         Console.WriteLine();
         return listA;
-
     }
 }
