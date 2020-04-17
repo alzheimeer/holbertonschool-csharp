02fafe48a959f4c2fd86f247d883a2a0d4e21327
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-16 19:32:51 -0500

    feature 14

--- 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -6,7 +6,28 @@ namespace _14_rectangular_array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] mat = new int[5, 5];
+                    
+            for (int f = 0; f < 5; f++)
+            {
+                for (int c = 0; c < 5; c++)
+                {
+                    if(f == 2 && c == 2)
+                        mat[f, c] = 1;
+                    else
+                        mat[f, c] = 0;
+                }
+            }
+
+            for (int f = 0; f < 5; f++)
+            {
+                for (int c = 0; c < 5; c++)
+                {
+                    Console.Write(mat[f, c] + " ");
+                }
+                Console.WriteLine();
+            }
+           
         }
     }
 }
