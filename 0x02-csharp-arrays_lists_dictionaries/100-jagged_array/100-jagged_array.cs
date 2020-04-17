6a0f2fcb6eb276e8b74c3098a1c7db0595fd5d76
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-16 20:31:25 -0500

    feature adv

--- 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace _100_jagged_array
+class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
+	static void Main(string[] args)
+	{
+		int[][] jaggedArray = 
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
+            new int[] { 0, 1, 2, 3 },
+            new int[] { 0, 1, 2, 3, 4, 5, 6 },
+            new int[] { 0, 1 }
+        };
+                
+		for (int i = 0; i < jaggedArray.Length; i++)
+		{
+			for (int j = 0; j < jaggedArray[i].Length; j++)
+			{
+				Console.Write("{0}{1}", jaggedArray[i][j], j == (jaggedArray[i].Length - 1) ? "" : " ");
+                
+			}
+			Console.WriteLine();
+		}
+	}
 }
