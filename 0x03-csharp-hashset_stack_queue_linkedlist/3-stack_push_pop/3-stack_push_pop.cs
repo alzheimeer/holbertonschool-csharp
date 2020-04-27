56618653cc646e9591fb8e87192198da182c2e9c
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-27 18:54:01 -0500

    fix

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,30 +5,27 @@ class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-                int size = aStack.Count;
-        Console.WriteLine("Number of items: {0}", size);
-        if (size <= 0)
-        {
+        
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+
+        if (aStack.Count <= 0)
             Console.WriteLine("Stack is empty");
-        }
         else
-        {
-            string top = aStack.Peek();
-            Console.WriteLine("Top item: {0}", top);
-        }
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+
         if (aStack.Contains(search))
         {
-            Console.WriteLine("Stack contains \"{0}\": True", search);
+            Console.WriteLine("Stack contains {0}: True", search);
             while (aStack.Contains(search))
-            {
                 aStack.Pop();
-            }
-        }                
-        else
-        {
-            Console.WriteLine("Stack contains \"{0}\": False", search);
         }
+        else
+            Console.WriteLine("Stack contains {0}: False", search);
+
+        
         aStack.Push(newItem);
-        return aStack;
+
+        return (aStack);
+
     }
 }
