72a8adc412343a51c18e5890c49adf0df9bbae84
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-27 18:38:39 -0500

    feature task 3

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        string a = "";
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+        if (aStack.Count > 0)
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        else
+            Console.WriteLine("Stack is empty");
+        if (aStack.Contains(search))
+        {
+            a = "True";
+            while (aStack.Contains(search))
+                aStack.Pop();
+        }
+        else
+            a = "False"; 
+        Console.WriteLine("Stack contains {1}: {0}", a, search);
+        aStack.Push(newItem);
+
+        return (aStack);
+    }
+}
