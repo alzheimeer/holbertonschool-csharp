6742401ce8d887562c25e09aeb562251b267246b
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-27 14:53:33 -0500

    feature task 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        var newList = new List<int>();
+        foreach (int i in list1)
+            foreach (int j in list2)
+                if (i == j)
+                    newList.Add(i);
+        return(newList);
+    }
+}
