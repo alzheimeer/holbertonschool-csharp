ce83ca5ded178fe29bd8efc4ce31046ca1c14d27
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-27 17:31:36 -0500

    feature task 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        var newList = new List<int>();
+        var newList1 = new List<int>();
+        foreach (int i in list1)
+            foreach (int j in list2)
+                if (i == j)
+                    newList.Add(j);
+        list1.AddRange(list2);
+        list1.Sort();
+        
+        foreach(int x in list1)
+            if (newList.Contains(x) == false)
+                newList1.Add(x);
+
+        return(newList1);
+    }
+}
