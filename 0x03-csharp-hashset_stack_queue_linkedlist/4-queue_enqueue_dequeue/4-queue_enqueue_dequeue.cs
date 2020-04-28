2c96f409cab6c7ca8df95853c176670c1fd62edc
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-27 19:24:09 -0500

    feature task 4

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+
+        if (aQueue.Count <= 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine("Top item: {0}", aQueue.Peek());
+
+        if (aQueue.Contains(search))
+        {
+            Console.WriteLine("Stack contains \"{0}\": True", search);
+            while (aQueue.Contains(search))
+                aQueue.Dequeue();
+        }
+        else
+            Console.WriteLine("Stack contains \"{0}\": False", search);
+
+        
+        aQueue.Enqueue(newItem);
+        return (aQueue);
+    }
+}
