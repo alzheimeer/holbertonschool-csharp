a723a4fef1bc00990e0ee5cadaf948c71d8143ae
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-27 19:14:39 -0500

    fix

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,8 +5,7 @@ class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        int p = aStack.Count;
-        Console.WriteLine("Number of items: {0}", p);
+        Console.WriteLine("Number of items: {0}", aStack.Count);
 
         if (aStack.Count <= 0)
             Console.WriteLine("Stack is empty");
