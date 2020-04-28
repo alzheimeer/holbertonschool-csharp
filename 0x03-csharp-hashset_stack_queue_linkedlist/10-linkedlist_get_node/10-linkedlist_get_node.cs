40723747f9ee534b172c97a6249dc9a294b2d4a0
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-27 21:15:13 -0500

    feature task 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int h = 0;
+        if (myLList.Count < n )
+            return 0;
+        foreach(int i in myLList)
+            {
+                if (h == n)
+                    return i;
+                h += 1;
+            }
+        return 0;
+    }
+}
