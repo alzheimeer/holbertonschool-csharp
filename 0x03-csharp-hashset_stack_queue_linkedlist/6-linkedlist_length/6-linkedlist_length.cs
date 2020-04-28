c064665277f79d1a856cc1d32119d8a17867429e
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-27 19:59:51 -0500

    feature task 6

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        var n = 0;
+        foreach(int i in myLList)
+            n += 1;
+
+        return(n);
+        
+    }
+}
