c04db491d385c82c294748fce1bbe8ba5f14e2d5
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-27 20:27:56 -0500

    feature task 7

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        myLList.AddFirst(n);
+
+        return myLList.AddFirst;
+    }
+}
