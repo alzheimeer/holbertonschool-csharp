27e5cf7e3a55d25409fd896acda88b0a942427b9
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-27 19:51:24 -0500

    feature 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {   
+        if (size < 0)
+        {
+            LinkedList<int> listEmpty = new LinkedList<int>();
+            return listEmpty;
+        }
+        List<int> n = new List<int>();
+        for (var i = 0; i < size; i++)
+        {
+            n.Add(i);
+            Console.WriteLine(i);
+        }
+        LinkedList<int> s = new LinkedList<int>(n);
+        return s;
+        
+    }
+}
