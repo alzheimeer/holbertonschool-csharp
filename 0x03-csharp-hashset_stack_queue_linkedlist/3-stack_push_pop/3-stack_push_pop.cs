8450bb626ca7778aea0ebe550d71f9a9be7e5b06
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-27 19:12:53 -0500

    fix

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -15,16 +15,15 @@ class MyStack
 
         if (aStack.Contains(search))
         {
-            Console.WriteLine("Stack contains {0}: True", search);
+            Console.WriteLine("Stack contains \"{0}\": True", search);
             while (aStack.Contains(search))
                 aStack.Pop();
         }
         else
-            Console.WriteLine("Stack contains {0}: False", search);
+            Console.WriteLine("Stack contains \"{0}\": False", search);
 
         
         aStack.Push(newItem);
-
         return (aStack);
 
     }
