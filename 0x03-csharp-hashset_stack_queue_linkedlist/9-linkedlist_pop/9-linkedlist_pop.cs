5219b91d7c88ed718edca3757018461fb27e28d7
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-27 20:56:04 -0500

    feature task 9

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count <= 0)
+            return 0;
+        var d = myLList.First.Value;
+        myLList.RemoveFirst();
+        return d;
+    }
+}
