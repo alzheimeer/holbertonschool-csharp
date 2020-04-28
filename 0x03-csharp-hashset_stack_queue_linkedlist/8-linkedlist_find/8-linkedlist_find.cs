24b3a7c8a3f5d19c9411c91e12e22d37f23d5973
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-27 20:45:01 -0500

    feature task 8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        var z = 0;
+        if(myLList.Contains(value))
+        {
+            foreach(int i in myLList)
+            {
+                if (i == value)
+                    return(z);
+                z += 1;
+            }
+        }
+        else
+        {
+            return(-1);
+        }
+        return -1;
+    }
+}
