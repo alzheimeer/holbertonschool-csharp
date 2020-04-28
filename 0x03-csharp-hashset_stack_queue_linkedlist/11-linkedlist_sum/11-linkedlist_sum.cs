98f66451b054c376283621907e63738fcf7ad055
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-27 21:19:56 -0500

    feature task 11

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int f = 0;
+        foreach(int i in myLList)
+            f += i;
+        return f;
+    }
+}
