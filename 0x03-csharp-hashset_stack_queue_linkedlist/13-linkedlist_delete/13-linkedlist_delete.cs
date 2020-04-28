1d4e8e92eb551a980a046378da201d0b887d3710
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-27 23:05:50 -0500

    feature task 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> current = myLList.First;
+        int x = 0;
+        while(current != null)
+        {
+            if(x == index)
+                myLList.Remove(current);
+            x += 1;
+            current = current.Next;
+        }
+    }
+}
