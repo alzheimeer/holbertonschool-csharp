b90d9c94f2812df396f30f1f1f80a840d5df4c8a
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-04-28 22:46:30 -0500

    fix task 3

--- 0x05-csharp-structs_enums/3-dog/3-dog.cs
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -17,7 +17,7 @@ struct Dog
 
         public override string ToString()
         {
-            return "Dog Name: " + name + "Age: " + age + "Owner: " + owner + "Rating: " + rating;
+            return String.Format("Dog Name: {0} \nAge: {1} \nOwner: {2} \nRating: {3}",name,age,owner,rating);
         }
     }
 
