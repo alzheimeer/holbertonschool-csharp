2d8cd9cbc9c9e4849868cb2bb08f3f7a1c2b8c97
alzheimeer <fogniebla@hotmail.com>
2020-05-04 15:34:38 -0500

    feature task 0

---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyMath
+{
+    ///<summary>method that adds 2 numbers</summary>
+    public class Operations
+    {
+        ///<summary>Function to add inputs.</summary>
+        public static int Add(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
