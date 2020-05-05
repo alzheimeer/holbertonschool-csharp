3651c149806fe82533e9e1b78ad8f6bd13d53dae
alzheimeer <fogniebla@hotmail.com>
2020-05-05 13:49:07 -0500

    fix

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,18 +1,50 @@
 using NUnit.Framework;
 
-namespace Tests
+namespace MyMath.Tests
 {
-    public class Tests
+    [TestFixture]
+    /// <summary>Tests Alz</summary>
+    public class OperationsTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void positiveInt()
         {
+            int result = Operations.Add(1, 9);
+
+            Assert.AreEqual(10, result);
         }
 
         [Test]
-        public void Test1()
+        public void negativeInt()
         {
-            Assert.Pass();
+            int result = Operations.Add(2, -12);
+
+            Assert.AreEqual(-10, result);
+        }
+        
+        [Test]
+        public void bothNegativeInt()
+        {
+            int result = Operations.Add(-10, -10);
+
+            Assert.AreEqual(-20, result);
+        }
+
+        [Test]
+        public void CeroInt()
+        {
+            int result = Operations.Add(0, -30);
+
+            Assert.AreEqual(-30, result);
+        }
+
+         [Test]
+        
+        public void BothCeroInt()
+        {
+            int result = Operations.Add(0, 0);
+
+            Assert.AreEqual(0, result);
         }
     }
 }
