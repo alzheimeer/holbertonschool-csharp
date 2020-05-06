a4e4ffb900415e775086484fc0be8e56a47e5d20
alzheimeer <fogniebla@hotmail.com>
2020-05-05 20:30:23 -0500

    feature task 3

--- 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -1,18 +1,38 @@
 using NUnit.Framework;
+using System.Globalization;
 
 namespace Text.Tests
 {
+    [TestFixture]
+    /// <summary>Tests Alz</summary>
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void xempty()
         {
-            Assert.Pass();
+            string s = "";
+            
+            bool result = Str.IsPalindrome(s);
+
+            Assert.AreEqual(true, result);
+        }
+        [Test]
+        public void xis()
+        {
+            string s = "Racecar";
+            
+            bool result = Str.IsPalindrome(s);
+
+            Assert.AreEqual(true, result);
+        }
+        [Test]
+        public void xnotis()
+        {
+            string s = "A man, a plan, a canal: Panama.";
+            
+            bool result = Str.IsPalindrome(s);
+
+            Assert.AreEqual(true, result);
         }
     }
 }
--- 0x07-csharp-tdd/3-palindrome/Text/Text.cs
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -1,8 +1,32 @@
 ﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace Text
 {
-    public class Class1
+    ///<summary>method verified palindrome</summary>
+    public class Str
     {
+        /// <summary>Max integer</summary>
+        /// <param name="s">string</param>
+        /// <returns>True if string is a palindrome, False if it’s not</returns>
+
+        public static bool IsPalindrome(string s)
+        {
+            string s2 = s.ToLower();
+            s2 = Regex.Replace(s2, "[:;,. \t\n\r]", "");
+            int Length = s2.Length - 1;
+            string reverse = "";  
+
+            while(Length>=0)  
+            {  
+                reverse = reverse + s2[Length];  
+                Length--;  
+            }
+            if (s2 == reverse)
+                return true;
+            else
+                return false;
+        }
     }
 }
