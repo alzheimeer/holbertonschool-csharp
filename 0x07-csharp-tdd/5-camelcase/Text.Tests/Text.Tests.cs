7e4ad0e0fc7be3db859b515f21e947f28544e299
alzheimeer <fogniebla@hotmail.com>
2020-05-05 23:17:47 -0500

    feature 5

--- 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -2,17 +2,36 @@ using NUnit.Framework;
 
 namespace Text.Tests
 {
+    [TestFixture]
+    /// <summary>Tests Alz</summary>
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void xfour()
         {
-            Assert.Pass();
+            string s = "";
+            
+            int result = Str.CamelCase(s);
+
+            Assert.AreEqual(0, result);
+        }
+        [Test]
+        public void xtwo()
+        {
+            string s = null;
+            
+            int result = Str.CamelCase(s);
+
+            Assert.AreEqual(0, result);
+        }
+        [Test]
+        public void xping()
+        {
+            string s = "papaNoelEsUnZombie";
+            
+            int result = Str.CamelCase(s);
+
+            Assert.AreEqual(5, result);
         }
     }
 }
--- 0x07-csharp-tdd/5-camelcase/Text/Text.cs
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -2,7 +2,23 @@
 
 namespace Text
 {
-    public class Class1
+    ///<summary>Each word begins with a capital letter except the first word</summary>
+    public class Str
     {
+        /// <summary>how many words are in a string</summary>
+        /// <param name="s">string</param>
+        /// <returns> number of words in s</returns>
+        public static int CamelCase(string s)
+        {
+            if (s == "" || s == null)
+                return 0;
+            int count = 0;
+            for (int i = 0; i <= s.Length-1; i++)
+            {
+                if (Char.IsUpper(s[i]))
+                    count++;
+            }
+            return count + 1;
+        }
     }
 }
