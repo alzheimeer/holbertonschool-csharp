550ced245fe232de4c995a4d8fb61ccbd7dd6025
Edgar <fogniebla@hotmail.com>
2020-05-26 22:31:47 -0500

    17 18

---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+///<summary>Class matrixmath</summary>
+class MatrixMath
+{
+	///<summary>matrix multiplication</summary>
+	///<return>matrix or matrix containing -1</return>
+	public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+	{
+		int row1 = matrix1.GetLength(0);
+		int col1 = matrix1.GetLength(1);
+		int row2 = matrix2.GetLength(0);
+		int col2 = matrix2.GetLength(1);
+        double[,] matrix3 = new double[row1, col2];
+        double[,] MT = {{-1}};
+
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
+            return MT;
+        else
+		{
+			for (int i = 0; i < row1; i++)
+				for (int j = 0; j < col2; j++)
+					for (int k = 0; k < col1; k++)
+						matrix3[i, j] += matrix1[i, k] * matrix2[k, j];
+			return matrix3;
+		}
+	}
+}
