4dbbfa7bd0be291096a71cc1874563f8f07c231d
Edgar <fogniebla@hotmail.com>
2020-05-27 23:03:41 -0500

    fix

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -8,14 +8,14 @@ class MatrixMath
 	public static double Determinant(double[,] matrix)
 	{
 		if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
-			return matrix[0][0] * matrix[1][1] - (matrix[0][1] * matrix[1][0]);
+			return ((matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]));
 		else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
 		{
-			double a = matrix[0][0] * (matrix[1][1] * matrix[2][2] - (matrix[1][2] * matrix[2][1]));
-			double b = matrix[0][1] * (matrix[1][0] * matrix[2][2] - (matrix[1][2] * matrix[2][0]));
-			double c = matrix[0][2] * (matrix[1][0] * matrix[2][1] - (matrix[1][1] * matrix[2][0]));
-			return a - b + c;
+			double a = matrix[0][0] * ((matrix[1][1] * matrix[2][2]) - (matrix[1][2] * matrix[2][1]));
+			double b = matrix[0][1] * ((matrix[1][0] * matrix[2][2]) - (matrix[1][2] * matrix[2][0]));
+			double c = matrix[0][2] * ((matrix[1][0] * matrix[2][1]) - (matrix[1][1] * matrix[2][0]));
+			return (a - b + c);
 		}
-		reeturn - 1;
+		return - 1;
 	}
 }
