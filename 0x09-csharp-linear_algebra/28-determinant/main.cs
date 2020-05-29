d84942562eb9901fd2f92901b4205b9af64f5c97
Edgar <fogniebla@hotmail.com>
2020-05-29 13:49:33 -0500

    fix

--- 0x09-csharp-linear_algebra/28-determinant/main.cs
+++ 0x09-csharp-linear_algebra/28-determinant/main.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        double[,] matrix = { { 3, -2 }, { 7, 4 } };
+        double[,] matrix = { { 1.2, -0.42 }, { 0.67, 2 } };
+
         Console.WriteLine(MatrixMath.Determinant(matrix));
     }
 }
