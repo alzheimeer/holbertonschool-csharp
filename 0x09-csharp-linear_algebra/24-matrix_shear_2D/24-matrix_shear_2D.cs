f30c9f3c8526cfd5bdad2acbb13cde74d5b35fc2
Edgar <fogniebla@hotmail.com>
2020-05-29 13:32:53 -0500

    fix

--- 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -7,20 +7,22 @@ class MatrixMath
     public static double[,] Shear2D(double[,] matrix, char direction, double factor)
     {
         double[,] newMatrix = new double[2,2];
-        double[,] shearMatrix = {
-            {1, factor},
-            {0, 1}
-        };
         double[,] MT = {{-1}};
-
-        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        double[,] shearMatrix;
+        if (direction == 'x' || direction == 'y')
+        {
+            if (direction == 'x')
+                shearMatrix = new double[,] {{1, 0}, {factor, 1}};
+            else
+                shearMatrix = new double[,] {{1, factor}, {0, 1}};
+        }
+        else
             return MT;
-        if (direction != 'x' && direction != 'y')
-            return MT;
-        newMatrix[0,0] = shearMatrix[0,0] * matrix[0,0] + shearMatrix[0,1] * matrix[0,1];
-        newMatrix[0,1] = matrix[0,1];
-        newMatrix[1,0] = shearMatrix[0,0] * matrix[1,0] + shearMatrix[0,1] * matrix[1,1];
-        newMatrix[1,1] = matrix[1,1];
-        return newMatrix;
+        
+        for (int i = 0; i < 2; i++)
+			for (int j = 0; j < 2; j++)
+				for (int k = 0; k < 2; k++)
+					newMatrix[i, j] += (matrix[i, k] * shearMatrix[k, j]);
+		return newMatrix;
     }
 }
---
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/main.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[,] matrix = { { 1, 2 }, { 3, 4 } };
+        double[,] result;
+        
+        result = MatrixMath.Shear2D(matrix, 'y', 3.45);
+        
+        for (int i = 0; i < result.GetLength(0); i++)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+                Console.Write(result[i, j]);
+                if (j != result.GetLength(1) - 1)
+                    Console.Write(", ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
