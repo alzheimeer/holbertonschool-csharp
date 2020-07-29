784d5ab8e5501a569c00c3edb9b9674b434f6616
Edgar <fogniebla@hotmail.com>
2020-07-29 18:12:51 -0500

    fix

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -3,32 +3,29 @@
 /// <summary>Create a public class</summary>
 class Player
 {
-    private string name;
-    private float maxHp;
-    private float hp;
+        private string name;
+        private float maxHp;
+        private float hp;
 
-    /// <summary> Constructor </summary>
-    /// <param name="name">The name of the player</param>
-    /// <param name="maxHp">The player's maximum hit points</param>
-    public Player(string name = "Player", float maxHp = 100f)
-    {
-
-        this.name = name;
-        this.hp = this.maxHp;
-        if (maxHp < 0)
+        /// <summary> Constructor </summary>
+        /// <param name="name">The name of the player</param>
+        /// <param name="maxHp">The player's maximum hit points</param>
+        public Player(string name = "Player", float maxHp = 100f)
         {
-            this.maxHp = 100f;
-            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+                if (maxHp <= 0f)
+                {
+                        this.maxHp = 100f;
+                        Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+                }
+                else
+                        this.maxHp = maxHp;
+                this.name = name;
+                this.hp = this.maxHp;
         }
-        else
-            this.maxHp = maxHp;
-    }
 
-    /// <summary>Method</summary>
-    public void PrintHealth()
-    {
-        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
-    }
-
+        /// <summary> Prints the player's health. </summary>
+        public void PrintHealth()
+        {
+                Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
+        }
 }
-
