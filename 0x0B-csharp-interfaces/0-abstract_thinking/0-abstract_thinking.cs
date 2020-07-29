6ffce3fd9022122a219c7712e10e3871c05145a4
Edgar <fogniebla@hotmail.com>
2020-07-28 20:05:05 -0500

    fix

--- 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,18 +1,13 @@
 ﻿using System;
 
-/// <summary>
-/// Abstract class Base
-/// </summary>
-public abstract class Base
+///<summary>Base class</summary>
+abstract class Base
 {
-  /// <summary>
-  /// given name of the object
-  /// </summary>
   public string name;
 
-  /// <summary>
-  /// Function ToString override
-  /// </summary>
-  /// <returns> returns a new string with name and its type</returns>
-  public override string ToString() => $"{name} is a {this.GetType()}";
+  /// <summary> Function ToString override </summary>
+   public override string ToString()
+    {
+        return (name + " is a " + GetType().Name);
+    }
 }
