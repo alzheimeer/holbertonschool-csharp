536d5508058b5f9977e2ad7272739bcdcaf7dce8
Edgar <fogniebla@hotmail.com>
2020-07-28 20:39:35 -0500

    feature task 1

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,12 +1,58 @@
 ﻿using System;
 
-namespace _1_user_interface
+///<summary>Base class</summary>
+abstract class Base
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+  public string name = "";
+
+	public override string ToString()
+	{
+		return (this.name + " is a " + this.GetType().ToString());
+	}
+}
+
+/// <summary> Interactive interface </summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+/// <summary> Breakable Interface </summary>
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+/// <summary> Collectable Interface </summary>
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+
+}
+
+/// <summary> Class that inherits from Base class and all interfaces </summary>
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+	public int durability { get; set; }
+	public bool isCollected { get; set; }
+
+    /// <summary> Takes place when the object interacts with something </summary>
+	public void Interact()
+	{
+
+	}
+
+    /// <summary> Takes place when the object brakes. </summary>
+	public void Break()
+	{
+
+	}
+
+    /// <summary> Takes place when the object collects something.</summary>
+	public void Collect()
+	{
+
+	}
 }
