13fece92a3413837d262325ebc32bf5a90d1d971
Edgar <fogniebla@hotmail.com>
2020-07-28 20:51:16 -0500

    feature task 2

--- 0x0B-csharp-interfaces/2-doors/2-doors.cs
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 
-namespace _2_doors
+///<summary>Base class</summary>
+abstract class Base
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+  public string name = "";
+
+	public override string ToString()
+	{
+		return (this.name + " is a " + this.GetType().ToString());
+	}
+}
+
+/// <summary> Interactive interface </summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+/// <summary> Breakable Interface </summary>
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+/// <summary> Collectable Interface </summary>
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+
+}
+
+/// <summary> Class that inherits from Base class and all interfaces </summary>
+class Door : Base, IInteractive
+{
+
+    public Door(string n = "Door")
+	{
+		name = n;
+	}
+
+    /// <summary> Takes place when the object interacts with something </summary>
+	public void Interact()
+	{
+        Console.WriteLine("You try to open the {0}. It's locked", name);
+	}
+
+  
 }
