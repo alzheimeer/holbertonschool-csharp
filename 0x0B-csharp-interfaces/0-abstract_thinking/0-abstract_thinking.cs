863f01c251f9ad80932304a0c40545f0454bd4f2
Edgar <fogniebla@hotmail.com>
2020-07-28 19:37:38 -0500

    feature task 0

--- 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 
+///<summary>abstract Base class</summary>
 abstract class Base
 {
 
     public string name = "";
  
-
+    /// <summary>Function ToString override</summary>
     public override string ToString()
     {
             return (name + " is a " + GetType().Name);
